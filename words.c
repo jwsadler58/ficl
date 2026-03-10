jwsadler58/ficl

commit 6c5e4e08a7c60b0518864b0999b132aeabf6fae0

refactor refill with bool

words.c
@@ -3613,11 +3613,11 @@ static void sourceid(FICL_VM *pVM)
 **************************************************************************/
 static void refill(FICL_VM *pVM)
 {
-    FICL_INT ret = (pVM->sourceID.i == -1) ? FICL_FALSE : FICL_TRUE;
-    if (ret && !pVM->fRestart)
+    bool flag = pVM->sourceID.i != -1;
+    if (!flag && !pVM->fRestart)
         vmThrow(pVM, VM_RESTART);
 
-    PUSHINT(ret);
+    PUSHINT(FICL_BOOL(flag));
     return;
 }
 

commit e9e6253065cdefc9650b4a983ff9c50d30573dcc

fix refill
broken by commit 6c5e4e08a7c60b0518864b0999b132aeabf6fae0

words.c
@@ -3614,7 +3614,7 @@ static void sourceid(FICL_VM *pVM)
 static void refill(FICL_VM *pVM)
 {
     bool flag = pVM->sourceID.i != -1;
-    if (!flag && !pVM->fRestart)
+    if (flag && !pVM->fRestart)
         vmThrow(pVM, VM_RESTART);
 
     PUSHINT(FICL_BOOL(flag));